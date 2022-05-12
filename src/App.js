da44ad4bb87e6b4b09a0b8e520b8a2eaca4a6f8f
Atish <atishbagate@gmail.com>
2022-05-13 00:10:30 +0530

    antd table added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 //components
 // import Test1 from './components/Test1';
 // import Test2 from './components/Test2';
-import ApexCharts from './components/ApexCharts.js'
+// import ApexCharts from './components/apexChart/ApexCharts.js'
+// import ApexCharts2 from './components/apexChart/ApexCharts2.js'
 
+import Table1 from './components/antd/Table1';
 function App() {
   return (
     <div className="App">
@@ -14,7 +16,9 @@ function App() {
       <div>
         {/* <Test1 /> */}
         {/* <Test2 /> */}
-        <ApexCharts />
+        {/* <ApexCharts /> */}
+         {/* <ApexCharts2 /> */}
+         <Table1 />
       </div>
     </div>
   );
